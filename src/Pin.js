dab40485a02e2ed2653ad88da123f1ed99971ade
poiresel <deepa@fluidity.io>
2019-05-19 08:21:32 -0400

    added icons

--- src/Pin.js
+++ src/Pin.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import constants from './constants.js';
 import { useSpring, animated } from 'react-spring';
+import cloud from "./lc.svg"
+import clear from "./c.svg"
+import snow from "./sn.svg"
+import rain from "./hr.svg"
 
 export function KittyPin({ kittyUrl }) {
-//    let radian = .10 * Math.PI
-//    const { radians } = useSpring({
-//      to: async next => {
-//        while (true) {
-//          await next({ radians: radian })
-//          radian = -radian
-//        }
-//      },
-//      from: { radians: radian },
-//      config: { duration: 200 },
-//      reset: false,
-//    })
+
+  let weather = "Light Cloud";
+  let weatherimage = null
+  if (["Snow", "Hail", "Sleet"].includes(weather)) {
+    // code block
+    weatherimage = snow
+  } else if (["Thunderstorm", "Heavy Rain", "Light Rain", "Showers"].includes(weather)) {
+    // code block
+    weatherimage = rain
+  } else if  (["Light Cloud", "Heavy Cloud"].includes(weather)) {
+      weatherimage = cloud
+  } else {
+      weatherimage = clear
+    }
     return (
-      <img 
+      <div>
+      <img
+        src={weatherimage}
+        alt={'cloud'}
+       />
+      <img
         src={kittyUrl}
         alt={'kitty'}
         style={{
           width: constants.kitty_width, height: constants.kitty_height
-        }} 
+        }}
       />
+      </div>
     );
   }
 
   const pinStyle = {
-    fill: '#d00',
+    fill: '#FF33E9',
     stroke: 'none'
   };
 
